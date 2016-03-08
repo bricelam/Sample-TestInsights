d1735d3617ba1f82a554348c7a67d42a31b71f96
Brice Lambson <bricelam@microsoft.com>
2016-03-08 14:58:17 -0800

    Stub a test importer

---
+++ src/TestInsights.Importer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace TestInsights.Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                if (!File.Exists(arg))
+                {
+                    Console.WriteLine("File '" + arg + "' does not exist.");
+
+                    continue;
+                }
+
+                Console.WriteLine("Importing '" + arg + "'...");
+
+                using (var reader = XmlReader.Create(arg))
+                {
+                    // TODO: Process documents conforming to http://xunit.github.io/docs/format-xml-v2.html
+                }
+            }
+        }
+    }
+}
