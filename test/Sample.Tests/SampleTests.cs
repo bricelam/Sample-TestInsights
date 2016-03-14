f044598c0d913e6383564d72a07526327812cc0a
Brice Lambson <bricelam@microsoft.com>
2016-03-14 12:43:46 -0700

    Add some sample tests

---
+++ test/Sample.Tests/SampleTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+[assembly: TestFramework("TestInsights.Reporter.InsightTestFramework", "TestInsights.Xunit")]
+
+namespace Sample.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void AlwaysPasses()
+        {
+            Assert.True(true);
+        }
+    }
+}
