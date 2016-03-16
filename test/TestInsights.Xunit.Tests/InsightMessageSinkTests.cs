a04ae2ab6b1ba201dc2ba46b7e315de4f3536735
Brice Lambson <bricelam@microsoft.com>
2016-03-16 14:33:54 -0700

    Test InsightMessageSink

---
+++ test/TestInsights.Xunit.Tests/InsightMessageSinkTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Data.Sqlite;
+using Moq;
+using TestInsights.Data;
+using Xunit;
+using Xunit.Abstractions;
+using SdkTestMessageVisitor = Xunit.Sdk.TestMessageVisitor;
+
+namespace TestInsights.Xunit
+{
+    public class InsightMessageSinkTests : IDisposable
+    {
+        private readonly string _connectionString = "" +
+            new SqliteConnectionStringBuilder
+            {
+                DataSource = Path.GetRandomFileName()
+            };
+        private readonly InsightContext _db;
+
+        public InsightMessageSinkTests()
+        {
+            _db = new InsightContext(_connectionString);
+        }
+
+        [Fact]
+        public void Adds_passed_tests()
+        {
+            var sink = new InsightMessageSink(
+                new SdkTestMessageVisitor(),
+                _connectionString);
+
+            var startTime = new DateTime(2015, 3, 16, 13, 23, 42);
+
+            var testAssemblyStarting = new Mock<ITestAssemblyStarting>();
+            testAssemblyStarting.Setup(m => m.StartTime).Returns(startTime);
+
+            var assemblyInfo = new Mock<IAssemblyInfo>();
+            assemblyInfo.Setup(m => m.Name).Returns("Assembly1");
+
+            var testAssembly = new Mock<ITestAssembly>();
+            testAssembly.Setup(m => m.Assembly).Returns(assemblyInfo.Object);
+
+            var typeInfo = new Mock<ITypeInfo>();
+            typeInfo.Setup(m => m.Name).Returns("Class1");
+
+            var testClass = new Mock<ITestClass>();
+            testClass.Setup(m => m.Class).Returns(typeInfo.Object);
+
+            var test = new Mock<ITest>();
+            test.Setup(m => m.DisplayName).Returns("Method1");
+
+            var testPassed = new Mock<ITestPassed>();
+            testPassed.Setup(m => m.TestAssembly).Returns(testAssembly.Object);
+            testPassed.Setup(m => m.TestClass).Returns(testClass.Object);
+            testPassed.Setup(m => m.Test).Returns(test.Object);
+            testPassed.Setup(m => m.ExecutionTime).Returns(3.14m);
+
+            sink.OnMessage(testAssemblyStarting.Object);
+            sink.OnMessage(testPassed.Object);
+            sink.OnMessage(Mock.Of<ITestAssemblyFinished>());
+
+            var result = _db.TestResults.Cast<TestPassedResult>().Single();
+            Assert.Equal("Assembly1", result.Assembly);
+            Assert.Equal("Class1", result.Class);
+            Assert.Equal("Method1", result.Name);
+            Assert.Equal(startTime, result.StartTime);
+            Assert.Equal(3.14m, result.ExecutionTime);
+        }
+
+        public void Dispose()
+        {
+            _db.Database.EnsureDeleted();
+            _db.Dispose();
+        }
+    }
+}
